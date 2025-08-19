5af20dd5860c92e447f32b5a6ce94c01ba2a3a1c
mineguy48 <toma.bercuci@gmail.com>
2025-08-19 15:00:54 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/treeweather/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.treeweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "🌳 Vremea: Cer senin și 25°C"
+    }
+}
